2b1bf6318b65dadc61ccd83a6df422017eedd70c
caylan wilcox <caylan45@yahoo.com>
2022-10-23 13:47:31 -0500

    commit tree 06dfaaad379d84da094c62a662f79bbede579a62

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Code your solutions in this file
+// Code your solutions in this f
+
+/*QUestions: 
+1. Do I need to set a parameter in the function write cards in order to receive arguments.
+// Why wont it let me return cards
+2. Why is write card function call faded out
+3. If I am creating the argument how will the tester file input their own argument
+4. Why doesn't events define if I use double quotes
+*/
+
+
+
+
+
+const cards = ["jake", "calvin", "walker"];
+const events = `birthday`;
+const newCards = [];
+//cardtype is the array of cards
+function writeCards(cards, events) {
+    for (let i = 0; i < cards.length; i++) {
+
+        newCards.push(`Thank you, ${cards[i]}, for the wonderful ${events} gift!`);
+
+    }
+    return newCards;
+
+}
+writeCards(newCards, events);
+
+
+let i=0;
+function countDown(i) {
+    while ( i >= 0) {
+       
+        console.log(i);
+        i--;
+    }
+}
+countDown(n);
